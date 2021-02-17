b9d5ff7aecc13605f0a32fe36c51e0f7a472bb68
Maxwell Young Robins <tug78217@temple.edu>
2021-02-16 19:37:16 -0500

    first commit

---
+++ src/main/java/edu/temple/signupform/FormActivity.java
@@ -0,0 +1,16 @@
+package edu.temple.signupform;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class FormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
